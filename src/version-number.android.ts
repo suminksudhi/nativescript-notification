d69abebe94a03459e56a20c41b702aea360e38d2
S795908 <suminkairali.sudhi@dnata.com>
2018-09-01 09:33:00 +0400

    sample config tests

--- src/version-number.android.ts
+++ src/version-number.android.ts
@@ -2,8 +2,8 @@ import * as application from "tns-core-modules/application";
 
 export class VersionNumber {
     get() {
-        var PackageManager = android.content.pm.PackageManager;
-        var pkg = application.android.context.getPackageManager().getPackageInfo(application.android.context.getPackageName(),
+        let PackageManager = android.content.pm.PackageManager;
+        let pkg = application.android.context.getPackageManager().getPackageInfo(application.android.context.getPackageName(),
             PackageManager.GET_META_DATA);
         return pkg.versionName;
     }
